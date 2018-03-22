c7701836678594142c22ce25487aad7c461d1e4b
carlonlv <36804238+carlonlv@users.noreply.github.com>
2018-03-21 20:54:13 -0400

    Update huffman.py

--- huffmantree/huffman.py
+++ huffmantree/huffman.py
@@ -522,8 +522,8 @@ def improve_tree(tree, freq_dict):
 
 
 if __name__ == "__main__":
-    import python_ta
-    python_ta.check_all(config="huffman_pyta.txt")
+    #import python_ta
+    #python_ta.check_all(config="huffman_pyta.txt")
     import doctest
     doctest.testmod()
 
